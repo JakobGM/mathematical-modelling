c553915f104acba9dd621784c99829f7e128b8dc
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2018-10-17 14:38:12 +0200

    tests: Add test dummy

---
+++ tests/test_dummy.py
@@ -0,0 +1,9 @@
+"""Dummy test to check if tests run properly."""
+
+import numpy as np
+
+
+def test_dummy():
+    """TODO: Delete this as soon as we have a proper test."""
+    null_vector = np.array([0, 0, 0])
+    assert not np.any(null_vector)
