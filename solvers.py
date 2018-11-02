d65a98a24fa23770dcb5fb317594f80648bc75bf
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2018-11-02 21:45:08 +0100

    solvers: Add glacier parameters class

--- solvers.py
+++ solvers.py
@@ -1,8 +1,69 @@
 """Numerical solvers made specifically for glacier dynamics. """
 
+from dataclasses import dataclass
+
 import matplotlib.pyplot as plt
 
 import numpy as np
+
+
+@dataclass
+class GlacierParameters:
+    """Dataclass containing relevant information regarding a glacier."""
+
+    # Initial height profile of glacier
+    h_0: np.ndarray
+
+    # And respective x-coordinates
+    xs: np.ndarray
+
+    # Accumulation rate of ice along the glacier
+    q: np.ndarray
+
+    # Slope of valley floor in radians
+    alpha: float
+
+    # Gravitational acceleration in m/s^2
+    g: float = 9.8
+
+    # Density of the glacier ice
+    rho: float = 917
+
+    # Material constant from Glen's law, usually in range [1.8, 5]
+    m: float = 3.0
+
+    # Another material property from Glen's law
+    # TODO: Find the typical value for this one!
+    mu: float = 1.0
+
+    def __post_init__(self) -> None:
+        """Calculate derived constants."""
+        # Max height of glacier
+        self.H: float = self.h_0.max()
+
+        # Length of glacier
+        # Assuming h > 0 for all x < x_F
+        self.L: float = self.xs[self.h_0 > 0][-1]
+
+        # Approximated to be a small parameter
+        self.epsilon: float = self.H / self.L
+
+        # Maximum accumulation rate
+        self.Q = self.q.max()
+
+        # Stress scaler
+        self.theta = self.rho * self.g * self.H * np.sin(self.alpha)
+
+        # Derived constants used in differential equation
+        self.kappa = (
+            2
+            * self.H
+            * self.mu
+            * (self.theta ** self.m)
+            * self.epsilon
+            / self.Q
+        )
+        self.lambda_ = self.kappa / (self.m + 2)
 
 
 class FiniteVolumeSolver:
--- tests/test_solvers.py
+++ tests/test_solvers.py
@@ -4,7 +4,7 @@ import numpy as np
 
 import pytest
 
-from solvers import FiniteVolumeSolver
+from solvers import FiniteVolumeSolver, GlacierParameters
 
 
 @pytest.fixture
@@ -20,3 +20,29 @@ def finite_volume_solver() -> FiniteVolumeSolver:
 def test_finite_volume_solver(finite_volume_solver):
     """Test plot method creating solution plot."""
     finite_volume_solver.plot(show=False)
+
+
+def test_glacier_parameters():
+    """GlacierParameters should contain relevant information."""
+    initial_height = np.array([1, 2, 3, 2, 1, 0, 0, 0]).astype(float)
+    x_coordinates = np.linspace(start=0, stop=700, num=8)
+    accumulation_rate = np.array([2, 2, 1, 0, -1, 0, 0, 0]).astype(float)
+    params = GlacierParameters(
+        h_0=initial_height,
+        xs=x_coordinates,
+        alpha=np.radians(3),
+        q=accumulation_rate,
+    )
+    assert params.g == 9.8
+    assert params.rho == 917
+    assert params.H == 3.0
+    assert params.L == 400.0
+    assert params.epsilon == 3.0 / 400.0
+    assert params.alpha == np.radians(3)
+    assert params.theta == 917 * 9.8 * 3 * np.sin(np.radians(3))
+    assert params.Q == 2.0
+
+    theta_cubed = (917 * 9.8 * 3 * np.sin(np.radians(3))) ** 3
+    kappa = 2 * 3 * 1 * theta_cubed * (3 / 400) / 2
+    assert params.kappa == kappa
+    assert params.lambda_ == kappa / 5
