solver: Fix simple accumulation model with scaling

solvers.py
@@ -24,11 +24,11 @@ class GlacierParameters:
     # And respective x-coordinates
     xs: np.ndarray
 
-    # Accumulation rate of ice along the glacier
-    q: np.ndarray
-
     # Slope of valley floor in radians
     alpha: float
+
+    # Accumulation rate of ice along the glacier
+    q: Optional[np.ndarray] = None
 
     # Length scaling for glacier in meters
     L: float = 1000.0
@@ -49,17 +49,24 @@ class GlacierParameters:
     # TODO: Find the typical value for this one!
     mu: float = 9.3e-25
 
+    # Simple accumulation model
+    q_0: Optional[float] = None
+    x_s: Optional[float] = None
+    x_f: Optional[float] = None
+
     def __post_init__(self) -> None:
         """Calculate derived constants."""
         # Approximated to be a small parameter
         self.epsilon: float = self.H / self.L
 
-        # Maximum accumulation rate
-        self.Q = np.abs(self.q).max()
-        self.q = PhysicalVariable(unscaled=self.q, scaled=self.q / self.Q)
-
         # Scale other variables
         self.xs = PhysicalVariable(unscaled=self.xs, scaled=self.xs / self.L)
+
+        if self.q is not None:
+            self.Q = np.abs(self.q).max()
+        else:
+            assert self.q_0
+            self.Q = self.q_0
 
         # Stress scaler
         self.theta = self.rho * self.g * self.H * np.sin(self.alpha)
@@ -75,6 +82,11 @@ class GlacierParameters:
         )
         self.lambda_ = self.kappa / (self.m + 2)
 
+        if self.q is None:
+            assert self.q_0 and self.x_s and self.x_f
+            self.create_simple_accumulation_model()
+        self.q = PhysicalVariable(unscaled=self.q, scaled=self.q / self.Q)
+
         if isinstance(self.h_0, (int, float)):
             self.h_0 = self.generate_steady_state_height(h_0=self.h_0)
         self.h_0 = PhysicalVariable(unscaled=self.h_0, scaled=self.h_0 / self.H)
@@ -82,22 +94,71 @@ class GlacierParameters:
     def generate_steady_state_height(self, h_0: float) -> np.ndarray:
         """Return height profile resulting in steady state, given q."""
         assert isinstance(h_0, (float, int))
+        h_0 = self.h_0 / self.H
         xs = self.xs.scaled
         q = self.q.scaled
         integrated_q = integrate.cumtrapz(y=q, x=xs, initial=0)
         integral_constant = self.lambda_ * h_0 ** (self.m + 2)
         integrated_q += integral_constant
         integrated_q[integrated_q < 0.0] = 0.0
-        return (integrated_q / self.lambda_) ** (1 / (self.m + 2))
+        return (integrated_q / self.lambda_) ** (1 / (self.m + 2)) * self.H
+
+    def create_simple_accumulation_model(self):
+        xs = self.xs.scaled
+
+        self.q_0 = PhysicalVariable(unscaled=self.q_0, scaled=self.q_0 / self.Q)
+        q_0 = self.q_0.scaled
+
+        self.x_f = PhysicalVariable(unscaled=self.x_f, scaled=self.x_f / self.L)
+        x_f = self.x_f.scaled
+
+        self.x_s = PhysicalVariable(unscaled=self.x_s, scaled=self.x_s / self.L)
+        x_s = self.x_s.scaled
+
+        if isinstance(self.h_0, np.ndarray):
+            h_0 = self.h_0[0] / self.H
+        elif isinstance(self.h_0, (int, float)):
+            h_0 = self.h_0 / self.H
+        else:
+            raise ValueError
+
+        slope = (
+            -2
+            * (q_0 * x_f + (self.lambda_ * h_0) ** (self.m + 2))
+            / (x_f - x_s) ** 2
+        )
+
+        num = len(xs)
+        stop = xs[-1]
+        q = np.zeros(num)
+        dx = stop / (num - 1)
+        snow_line_index = int(x_s / dx)
+        q[: snow_line_index - 1] = q_0
+
+        tongue_index = int(x_f / dx)
+        slope_index_rate = slope * dx
+        q[snow_line_index - 1 : tongue_index] = (
+            slope_index_rate * np.arange(tongue_index - snow_line_index + 1)
+            + q_0
+        )
+        self.q = self.Q * q
 
     def plot(self, show: bool = True) -> plt.Axes:
         fig, ax = plt.subplots(1, 1)
         ax.set_title('Initial conditions')
         ax.set_xlabel('$x$')
         ax.set_ylabel('$z$')
+        xs = self.xs.unscaled
+        hs = self.h_0.unscaled
 
-        ax.fill([0, *self.xs.unscaled], [0, *self.h_0.unscaled])
+        ax.fill([0, *xs, xs[-1]], [0, *hs, 0], alpha=0.7)
         ax.legend(['Glacier'])
+        ax.set_xlim(0, xs[-1])
+
+        ax2 = ax.twinx()
+        ax2.plot(self.q.unscaled, color='tab:red', alpha=0.7)
+        ax2.set_ylabel('$q$')
+        ax2.legend(['Accumulation rate'], loc='lower right')
 
         if show:
             plt.show()
@@ -176,30 +237,3 @@ class FiniteVolumeSolver:
             plt.show()
 
         return fig
-
-
-def simple_accumulation_model(
-    snow_line: float,
-    tongue: float,
-    permanent_snow_rate: float,
-    stop: float,
-    num: int,
-    h_0: float = 0,
-) -> Tuple[np.ndarray, np.ndarray]:
-    xs = np.linspace(start=0, stop=stop, num=num)
-    slope = (
-        -2 * (permanent_snow_rate * tongue + h_0) / (tongue - snow_line) ** 2
-    )
-
-    q = np.zeros(num)
-    dx = stop / (num - 1)
-    snow_line_index = int(snow_line * dx)
-    q[: snow_line_index - 1] = permanent_snow_rate
-
-    tongue_index = int(tongue * dx)
-    slope_index_rate = slope * dx
-    q[snow_line_index - 1 : tongue_index] = (
-        slope_index_rate * np.arange(tongue_index - snow_line_index + 1)
-        + permanent_snow_rate
-    )
-    return xs, q

tests/test_solvers.py
@@ -4,11 +4,7 @@ import numpy as np
 
 import pytest
 
-from solvers import (
-    FiniteVolumeSolver,
-    GlacierParameters,
-    simple_accumulation_model,
-)
+from solvers import FiniteVolumeSolver, GlacierParameters
 
 
 @pytest.fixture
@@ -26,25 +22,11 @@ def finite_volume_solver() -> FiniteVolumeSolver:
     return FiniteVolumeSolver(glacier)
 
 
-def test_simple_accumulation_model():
-    _, q = simple_accumulation_model(
-        snow_line=3, tongue=9, permanent_snow_rate=2, num=11, stop=10
-    )
-    np.testing.assert_array_equal(
-        q, np.array([2, 2, 2, 1, 0, -1, -2, -3, -4, 0, 0])
-    )
-
-
 def test_plotting_initial_conditions():
-    xs, q = simple_accumulation_model(
-        snow_line=300,
-        tongue=600,
-        stop=1000,
-        permanent_snow_rate=1,
-        num=1001,
-        h_0=50,
+    xs = np.arange(0, 1001).astype(float)
+    glacier = GlacierParameters(
+        h_0=10000, xs=xs, alpha=np.radians(3), q=xs, x_f=600, x_s=300
     )
-    glacier = GlacierParameters(h_0=50, xs=xs, q=q, alpha=np.radians(3))
     glacier.plot(show=False)
 
 